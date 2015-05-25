443244ecfecd746f27fe93ad0999225971361ce7
Arnaud Puigserver <arnaud.puigserver@ensea.fr>
2015-05-25 18:18:30 +0200

    Permier Commit d'initialisation du projet + Requette de recherhce.

---
+++ src/java/managedBeans/RequestBean.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedBeans;
+
+import java.time.LocalDate;
+import java.util.Date;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author arnaudpuigserver
+ */
+@ManagedBean
+@SessionScoped
+public class RequestBean {
+
+    
+    private String departure;
+    private String destination;
+    private Date dateDepart;
+    private String dateRetour;
+    /**
+     * Creates a new instance of RequestBean
+     */
+    public RequestBean() {
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Date getDateDepart() {
+        return dateDepart;
+    }
+
+    public void setDateDepart(Date dateDepart) {
+        this.dateDepart = dateDepart;
+    }
+
+    public String getDateRetour() {
+        return dateRetour;
+    }
+
+    public void setDateRetour(String dateRetour) {
+        this.dateRetour = dateRetour;
+    }
+    
+}
